393fe08aaba86dc1887fd18a3aa8798bd71f7d45
Andrey Alibekov <alibekovand@gmail.com>
2023-06-22 23:37:45 +0700

    Bullet collision

--- Assets/Scripts/Game/Player/Bullet.cs
+++ Assets/Scripts/Game/Player/Bullet.cs
@@ -15,4 +15,14 @@ public class Bullet : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    { // at the end https://www.youtube.com/watch?v=JcfNFoeuzUk&list=PLx7AKmQhxJFajrXez-0GJgDlKELabQQHT&index=9 how to kill enemies
+        Debug.Log("collision");
+        if (other.GetComponent<PlayerMovement>() == null) // if it is not player
+        {
+            //Destroy(other.gameObject);
+            Destroy(gameObject); // for test (or not?)
+        }
+    }
 }
